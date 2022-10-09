b41e9f44644098caf261361df4efa6cbd120768b
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-10-08 22:47:29 -0500

    task service updated: get task, update task and delete task by id added

--- src/services/task.js
+++ src/services/task.js
@@ -14,3 +14,33 @@ export const getTasks = (limit = 10, offset = 0) => {
     offset,
   });
 };
+
+export const getTaskById = (id) => {
+  return Task.findOne({
+    where: {
+      id,
+      states: ["completed", "pending"],
+    },
+  });
+};
+
+export const updateTaskById = (id, updatedTask) => {
+  return Task.update(updatedTask, {
+    where: {
+      id,
+      states: ["completed", "pending"],
+    },
+  });
+};
+
+export const deleteTaskById = (id) => {
+  return Task.update(
+    { states: "deleted" },
+    {
+      where: {
+        id,
+        states: ["completed", "pending"],
+      },
+    }
+  );
+};
