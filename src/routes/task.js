8e7a70ff5636c106737a8156576a0c093d92afa7
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-10-08 22:21:52 -0500

    task routes updated: get tasks added

--- src/routes/task.js
+++ src/routes/task.js
@@ -4,5 +4,6 @@ import * as taskController from "../controllers/task";
 const router = Router();
 
 router.post("/", taskController.createTask);
+router.get("/", taskController.getTasks);
 
 export default router;
