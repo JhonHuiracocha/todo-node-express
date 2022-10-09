e38a257bdc74c801339365640dfc2d06b21d4b8f
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-10-08 22:47:58 -0500

    task routes updated: get task, update task and delete task by id added

--- src/routes/task.js
+++ src/routes/task.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.post("/", taskController.createTask);
 router.get("/", taskController.getTasks);
+router.get("/:id", taskController.getTask);
+router.patch("/:id", taskController.updateTaskById);
+router.delete("/:id", taskController.deleteTaskById);
 
 export default router;
