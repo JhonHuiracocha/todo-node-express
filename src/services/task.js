ddafd597f14a6f0d91db237ff9fe7e6044543e7a
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-10-08 22:20:18 -0500

    task service updated: get tasks added

--- src/services/task.js
+++ src/services/task.js
@@ -1,3 +1,16 @@
 import { Task } from "../models/task";
 
-export const createTask = (task) => Task.create(task);
+export const createTask = (task) => {
+  return Task.create(task);
+};
+
+export const getTasks = (limit = 10, offset = 0) => {
+  return Task.findAll({
+    where: {
+      states: ["completed", "pending"],
+    },
+    order: [["createdAt", "DESC"]],
+    limit,
+    offset,
+  });
+};
